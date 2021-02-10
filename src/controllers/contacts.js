c62179770cf6561d3f60c0654042ffc9093e1c29
Joe Britton <joebritton@marlowefireandsecurity.com>
2021-02-10 11:31:45 +0000

    db seems to be setup. All functionality is in place. All tested.

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,26 +1,60 @@
-import { Contact } from '../models/Contact'
+const {Contact} = require("../models/Contact");
 
 
 const addContact = async (req, res) => {
     try {
         const contact = new Contact(req.body);
-        const savedContact = await contact.save;
-        res.status(201).send({savedContact});
+        const savedContact = await contact.save();
+        res.status(201).send(savedContact);
     } catch (error) {
         if (error.code === 11000) {
             res.status(400).send({message: "Contact already exists"});
         } else {
             res.status(500).send({ message: "Could not connect"});
         }
-        
     }
 };
 
 const deleteContact = async (req, res) => {
     try {
-        const contact = await Contact.findByIdAndDelete(req.body.id)
+        const contact = await Contact.findByIdAndDelete(req.params.id)
         res.status(200).send(contact);
     } catch (error) {
         res.status(404).send({message: "User not found"});
     }
 };
+
+const updateContact = async (req, res) => {
+    try {
+        const contact = await Contact.findByIdAndUpdate(req.params.id, req.body, {new:true});
+        res.status(200).send({message: "Update Successful"})
+    } catch (error) {
+        res.status(404).send({message: "Failed to update contact"})
+    }
+}
+
+const getContact = async (req, res) => {
+    try {
+        const contact = await Contact.findById(req.params.id);
+        res.status(200).send(contact);
+    } catch (error) {
+        res.status(404).send({message: "Failed to get contact"});
+    }
+};
+
+const getContacts = async (req, res) => {
+    try {
+        const contact = await Contact.find({});
+        res.status(200).send(contact);
+    } catch (error) {
+        res.status(404).send({message: "Failed to get all contacts"});
+    }
+}
+
+module.exports = {
+    addContact,
+    deleteContact,
+    updateContact,
+    getContact,
+    getContacts,
+}
--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,11 +1,12 @@
 const {Router} = require("express");
-const {getContacts, getContact, addContact, deleteContact} = require("../controllers/contacts");
+const {getContacts, getContact, addContact, deleteContact, updateContact} = require("../controllers/contacts");
 const contactsRouter = Router();
 
 contactsRouter.get("/contacts", getContacts);
 contactsRouter.get("/contacts/:id", getContact);
 contactsRouter.post("/contacts", addContact);
 contactsRouter.delete("/contacts/:id", deleteContact);
+contactsRouter.patch("/contacts/:id", updateContact)
 
 module.exports = {
     contactsRouter,
